83ec1bf8ea77653e00ddeae3bc3d82ce660ed303
wangdahoo <157195705@qq.com>
2017-02-27 01:18:08 +0800

    update

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="page has-navbar" v-nav="{ title: 'About', showBackButton: true }">
+  <div class="page has-navbar" v-nav="{ title: 'About', showBackButton: true, onBackButtonClick: back }">
     <div class="page-content text-center">
       <h2 class="padding">Nothing here.</h2>
     </div>
   </div>
 </template>
+<script>
+  export default {
+    methods: {
+      back() {
+        $router.back('/')
+      }
+    }
+  }
+</script>
